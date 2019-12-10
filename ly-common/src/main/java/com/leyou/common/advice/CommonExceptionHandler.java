f58dc2f653a02c1eda8f5a9c047bde40184e0481
hanxiao <1098269542@qq.com>
2019-12-10 23:22:25 +0800

    1.删除无用注解

--- ly-common/src/main/java/com/leyou/common/advice/CommonExceptionHandler.java
+++ ly-common/src/main/java/com/leyou/common/advice/CommonExceptionHandler.java
@@ -3,9 +3,7 @@ package com.leyou.common.advice;
 import com.leyou.common.enums.ExceptionEnum;
 import com.leyou.common.exception.LyExcetion;
 import com.leyou.common.vo.ExceptionResult;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
